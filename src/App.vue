dc61c4778e04230120fbaabe3cff55e0555619cb
杨宏超 <9469647+vaeyhc@user.noreply.gitee.com>
2023-05-21 19:43:25 +0800

    elementPlus主题定制完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
 </template>
 
 <script>
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
+// 导入对应包
+import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,12 +16,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      resolvers: [ElementPlusResolver({ importStyle : 'sass' })],
+    }),
+       // 按需定制主题配置
+       ElementPlus({
+        useSource: true,
+      }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
