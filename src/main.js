9f5e0f58f883176f4c23bedb1677e628244c6c48
杨宏超 <9469647+vaeyhc@user.noreply.gitee.com>
2023-05-21 19:19:58 +0800

    elementPlus自动导入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
+
 
 app.mount('#app')
